646c003029151e9a50768d55382d116d37533f33
Eero Savukoski <eero.savukoski@edu.tampere.fi>
2019-10-23 14:26:40 +0300

    23.10.2019

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const [notes, setNotes] = useState([]);
+  const getNotes = () => {
+  console.log("Starting effect");
+  axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    //const notes = response.data
+    console.log("promise fulfilled");
+    setNotes(response.data);
+  })};
+
+  useEffect(getNotes, []);
+
+  console.log("ready", notes);
+
+  // testing addnote
+  const addNote = event => {
+  const testNote = {
+    content: "Remember to sleep",
+    date: "2019-10-23T14:06:00.000Z",
+    important: true
+  };
+  axios
+  .post('http://localhost:3001/notes', testNote)
+  .then(response => {
+    console.log(response);
+  })
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>JSON server with notes</h1>
       </header>
+      <button onClick={e => addNote(e)}>Click me</button>
     </div>
   );
 }
